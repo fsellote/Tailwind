b1dd9be9af35472ddc58b60a4d54677d3219ac09
Francen <sellote.francen519@gmail.com>
2024-05-15 09:51:45 +0800

    Tailwind CSS React

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <div>
+      <h1>
+        Hello, Tailwind CSS in React!
+      </h1>
+    </div>
+    </div>
+  );
+}
+
+export default App;
